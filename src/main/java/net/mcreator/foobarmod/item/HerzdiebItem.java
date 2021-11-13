0cada7487372e61aae9ec6843aa5df19f0aa4540
cwege <cwege@web.de>
2021-11-13 11:06:47 +0100

    Julis neue Blöcke

---
+++ src/main/java/net/mcreator/foobarmod/item/HerzdiebItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.foobarmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.foobarmod.FoobarmodModElements;
+
+@FoobarmodModElements.ModElement.Tag
+public class HerzdiebItem extends FoobarmodModElements.ModElement {
+	@ObjectHolder("foobarmod:herzdieb")
+	public static final Item block = null;
+	public HerzdiebItem(FoobarmodModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 13f;
+			}
+
+			public int getHarvestLevel() {
+				return 15;
+			}
+
+			public int getEnchantability() {
+				return 10;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("herzdieb"));
+	}
+}
